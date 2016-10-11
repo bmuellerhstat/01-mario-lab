98e333d0443f585c5262dfd904ef506a27b215bd
meitingi2005 <meitingi2005@hstat.org>
2016-10-11 14:46:19 +0000

    HW 2.0

--- pyramid.rb
+++ pyramid.rb
@@ -1,13 +1,16 @@
 
 # part 1
 
-def pyramid
+
+def method 
     puts "
     # #
   ## ##
  ### ###
 #### ####"
 end
+
+method
 
 
 
@@ -28,19 +31,10 @@ method(userNum)
 	
 	
 # part 3	
-	def method((num,chain))
-	
-puts "How tall?"
-userNum= gets.chomp.to_i
-
-	
-puts "how many chain?"
-chain= gets.chomp.to_i
+		def method(num,chain)
 
 chain.times do
 	nn=1
-	num=userNum 
-	
 num.times do
 	
 	puts " "*(num-nn)+ "#" * (nn) + " " + "#" * (nn)
@@ -51,9 +45,18 @@ end
 end 
 
 end
+puts "How tall?"
+userNum= gets.chomp.to_i
 
 	
+puts "how many chain?"
+userchain= gets.chomp.to_i
+	
 method(userNum,userchain)
+
+
+	
+
 
 
 	
@@ -73,15 +76,16 @@ method(userNum,userchain)
      
  	if method == true
      userNum.times do
-     	puts " "*(nn-1)+ "#" * (mm) + " " + "#" * (mm)
-      	
+    
+      	puts " "*(mm-1)+ "#" * (nn) + " " + "#" * (nn)
       nn = nn+ 1
       mm = mm- 1
      method = false
      end
  	else
     userNum.times do
-    puts " "*(mm-1)+ "#" * (nn) + " " + "#" * (nn)
+    		puts " "*(nn-1)+ "#" * (mm) + " " + "#" * (mm)
+   
 
      nn = nn+ 1
       mm = mm- 1
