72f6e6c2f7e90b3ee90ea01bc73243237a221cfb
daysip5272 <daysip5272@hstat.org>
2016-10-11 17:48:18 +0000

    try

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,24 @@
 #write your code here
+def tall
+print "how tall?"
+tall= gets.chomp;
+end
+def mario
+ o="#"
+n= tall
+n.times { |i|puts " "*(n-i)+o*i + "  " + o*i }
+end    
+
+
+def tall
+print "how tall?"
+tall= gets.chomp;
+print "how wide"
+wide = gets.chomp;
+end
+
+def size
+o="#"*wide
+n= tall
+n.times { |i|puts " "*(n-i)+o*i + "  " + o*i }
+end    
