cd86c8f8ff17d0eb0f8ca827287d0fd7fe18b61c
Ahmed Abdalla <ahmeda1955@hstat.org>
2016-10-11 15:58:34 +0000

    3

--- pyramid.rb
+++ pyramid.rb
@@ -10,4 +10,11 @@ end
 def tall
    puts "How tall do you want your pyramid?"
    height=gets.chomp.to_i 
-  
+    x = 1
+    y=height
+    height.times {
+     puts " " *(y - 1)+'#' * x +"  "+"#" * x
+     x += 1
+     y -= 1
+    }
+end
