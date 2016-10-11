99fce86e8716ca8c119f4077eea3047b9a076a01
jeremye3180 <Jeremye3180@hstat.org>
2016-10-11 15:39:05 +0000

    finsih part 1,2,3

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,34 @@
 #write your code here
+def pyramid()
+puts "   #   #"
+puts "  ##   ##"
+puts " ###   ###"
+puts "####   ####"
+end
+
+
+
+pyramid
+
+def m_part2()
+   n = 1
+   print "How tall do you want the pyramid?"
+  height = gets.chomp.to_i
+
+while height > 0 do
+
+puts " "*(height-1) + "#"*(n) + "  " + "#"*(n)+ " "*(height-1)
+ n += 1
+height -= 1
+end
+end
+m_part2()
+
+def m_part3()
+   print "how many pyramids do you want?"
+   height = gets.chomp.to_i
+   
+   height.times{puts pyramid}
+   
+end
+m_part3()
