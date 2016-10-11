703a5ea4e7fdd502f7cf1dfad7bf3f4ab4e97dac
Gabriel Charriez <gabrielc0464@hstat.org>
2016-10-11 14:23:37 +0000

    part 3 i tried

--- pyramid.rb
+++ pyramid.rb
@@ -12,32 +12,30 @@ end
 mario()
 
 def mario_loop()
-    help = 1
-    print "What height do you want the pyramid to be?"
-   height = gets.chomp.to_i
+   help = 1
+   print "What height do you want the pyramid to be?"
+  height = gets.chomp.to_i
 
- while height > 0 do
+while height > 0 do
 
 puts " "*(height-1) + "#"*(help) + "  " + "#"*(help)+ " "*(height-1)
-  help += 1
- height -= 1
- end
- end
- mario_loop()
- 
- mario()
+ help += 1
+height -= 1
+end
+end
+mario_loop()
 
-def mario_2()
-    pls = 1
-    print "What height do you want the pyramid to be?"
-   high = gets.chomp.to_i
 
- while high > 0 do
 
-puts " "*(high-1) + "#"*(pls) + "  " + "#"*(pls)+ " "*(high-1)
-  pls += 1
- high -= 1
+
+def chaining
+   t = 1
+  puts "How tall do you want the blocks to be?"
+  y = gets.chomp.to_i
+  y.times {
+     puts " " *(y - 1)+'#' * t +"  "+"#" * t
+      y=y - 1
+     t=t + 1
+  }
  end
- end
- mario_2()
- 
+ chaining()
