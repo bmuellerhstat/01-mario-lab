c63c53c2af1709460e7f317e143600291ff77350
derekc4859 <derekc4859@hstat.org>
2016-10-11 14:02:37 +0000

    Pull request

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,66 @@
 #write your code here
+
+
+
+ def pyramid
+   # FIRST METHOD
+    # puts "   # # "
+    # puts "  ## ##"
+    # puts " ### ### "
+    # puts "#### #### "
+
+ # SECOND METHOD
+ #  puts "Hi, how tall would you like your pyramid to be?"
+ #  number = gets.chomp
+ 
+ #  if number.to_i <= 23
+ #  number.to_i.times {|tall|
+ #  puts " " * (23 - tall) +  "#" * (1 + tall) + " " + "#" * (1 + tall) 
+ #  }
+ #    else
+ #  puts "I can't do that, sorry."
+ #  end
+ #  end
+
+ # THIRD METHOD
+#  puts "Hi, how tall would you like your pyramid to be?"
+#  number = gets.chomp
+#  puts "How many pyramids do you want?"
+#  more_pyramids = gets.chomp
+ 
+#  if number.to_i <= 23
+#  number.to_i.times { 
+#  more_pyramids.to_i.times{|tall|
+#  puts " " * (23 - tall) +  "#" * (1 + tall) + " " + "#" * (1 + tall) 
+#  }
+#  }
+#    else
+#  puts "I can't do that, sorry."
+#  end
+#  end
+
+ puts "Hi, how tall would you like your pyramid to be?"
+ number = gets.chomp
+ puts "How many pyramids do you want?"
+ more_pyramids = gets.chomp
+ 
+ if number.to_i <= 23
+ number.to_i.times { 
+       if more_pyramids.to_i % 2 == 0
+         more_pyramids.to_i.times{|tall|
+            puts " " * (tall + 23) +  "#" * (23 - tall) + " " + "#" * (23 - tall) 
+             puts " " * (23 - tall) +  "#" * (1 + tall) + " " + "#" * (1 + tall) 
+                      }
+                        
+                else
+                  more_pyramids.to_i.times{|tall|
+                     puts " " * (23 - tall) +  "#" * (1 + tall) + " " + "#" * (1 + tall) 
+                      }
+          end          
+ } 
+   else
+ puts "I can't do that, sorry."
+ end
+ end
+ pyramid
+
