dcc5537c175bec8a5b12a19de813e1d4f478de90
jessicaw9494 <jessicaw9494@hstat.org>
2016-10-11 18:06:49 +0000

    finish lab

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,21 @@
 #write your code here
+def pyramid
+   "   #  #   
+  ##  ##  
+ ###  ### 
+####  ####"
+end
+# pyramid
+
+def tall
+    puts "How tall they would like to make the pyramid?"
+    height=gets.chomp.to_i 
+    x = 1
+height.times {
+  puts "#{" " *(height - 1)}#{'#' * x }  #{"#" * x}"
+  x += 1
+  height -= 1
+}
+
+end
+tall
