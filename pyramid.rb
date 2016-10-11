be7e83098a419d39bc019489614d1007938706cb
Ahmed Abdalla <ahmeda1955@hstat.org>
2016-10-11 16:04:18 +0000

    kinda done

--- pyramid.rb
+++ pyramid.rb
@@ -7,7 +7,7 @@ def pyramid
 end
 # pyramid
 
-def tall
+def long
    puts "How tall do you want your pyramid?"
    height=gets.chomp.to_i 
     x = 1
@@ -18,3 +18,21 @@ def tall
      y -= 1
     }
 end
+l
+
+def repeat
+   puts "How tall do you want your pyramid?"
+   height=gets.chomp.to_i 
+   puts "repeat?"
+   repeat=gets.chomp.to_i
+repeat.times{
+    x = 1
+    y=height
+    height.times {
+     puts " " *(y - 1)+'#' * x +"  "+"#" * x
+     x += 1
+     y -= 1
+    }
+}
+end
+repeat
