b58b207cc84486d4b36ece99914c1090bf1e913a
Jenny Feng <jennyfeng.ga.ling1999@gmail.com>
2016-10-12 14:20:58 +0000

    fix part one

--- pyramid.rb
+++ pyramid.rb
@@ -1,1 +1,17 @@
 #write your code here
+def mario_blocks(height)
+    level = 1
+    height.times do 
+        puts " "*(height-level) + "#"*level + " " + "#"*level
+        level += 1
+    end
+end
+
+def tall_question
+    puts "How tall would you like to make your pyramid?"
+    #height = gets.chomp
+end
+
+def chained_pyramids
+    puts ""
+end
